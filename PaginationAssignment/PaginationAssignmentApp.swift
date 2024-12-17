ec239373fe7839bb8cf0bc1de9b94232aaf08e0f
Ali <m.ali.rezaeiii09@gmail.com>
2024-12-17 21:08:36 +0330

    Initial Commit

---
+++ PaginationAssignment/PaginationAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaginationAssignmentApp.swift
+//  PaginationAssignment
+//
+//  Created by Ali on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct PaginationAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
